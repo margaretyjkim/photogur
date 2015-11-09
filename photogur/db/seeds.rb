d2f4aac9dceef66456dd124e48b14a37bd82b8c7
margaretyjkim <margaret.yj.kim@gmail.com>
2015-11-09 14:28:20 -0500

    Making strawberry pictures show at /pictures

--- photogur/db/seeds.rb
+++ photogur/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Picture.create!(
+  :title  => "Colored Strawberries",
+  :artist => "Bill",
+  :url    => "http://www.lovethispic.com/uploaded_images/15792-Colored-Strawberries.jpg"
+)
+
+Picture.create!(
+  :title  => "How to Prepare Strawberries",
+  :artist => "Produce Made Simple",
+  :url    => "http://producemadesimple.ca/wp-content/uploads/2013/07/how-to-prepare-strawberries.jpg"
+)
+
+Picture.create!(
+  :title  => "Chocolate Covered Strawberries Made In An Ice Cube Tray",
+  :artist => "Haley",
+  :url    => "http://www.cheaprecipeblog.com/wp-content/uploads/2012/10/Chocolate-Strawberries-In-An-Ice-Cube-Tray-2.jpg"
+)
